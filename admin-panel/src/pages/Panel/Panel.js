315db72a97486d284368eb2460e49e126fc660d3
Volodymyr Yastrubchak <volodymyr.yastrubchak@gmail.com>
2020-12-23 17:29:59 +0200

    Added Panel Navigation

--- admin-panel/src/pages/Panel/Panel.js
+++ admin-panel/src/pages/Panel/Panel.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React from "react";
+import { TabsContainer } from "../../containers/TabsContainer";
 
 export const Panel = () => {
-    return (
-        <div>
-            Panel
-        </div>
-    )
-}
+  return (
+    <div style={{ padding: "20px 0px" }}>
+      <TabsContainer />
+    </div>
+  );
+};
